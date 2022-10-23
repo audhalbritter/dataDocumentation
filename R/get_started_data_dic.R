eb507c81614a8371faa2c023f6406dd78d45006a
audhalbritter <me@audhalbritter.com>
2022-10-23 08:23:07 +0200

    add example to get started and path

--- R/get_started_data_dic.R
+++ R/get_started_data_dic.R
@@ -1,5 +1,6 @@
 #' Make data dictionary directory and description file
 #' @description Function to make a new directory for the data dictionary directory and a template for the description file. This function should be run first, before making a data dictionary. It only has to be run once.
+#' @param path path file for creating directory for data dictionary
 #'
 #' @return a directory and a empty description table
 #'
@@ -7,16 +8,17 @@
 #' @importFrom readr write_csv
 #'
 #' @examples
-#' get_started()
+#' temp <- tempdir()
+#' get_started(path = temp)
 #'
 #' @export
 #'
 
-# A fu
-get_started <- function(){
+# A function to create a directory and
+get_started <- function(path = "data_dic"){
 
   # create a directory data dic
-  dir.create("data_dic")
+  dir.create(path)
 
   # make template for description table
   description_table <- tibble(TableID = character(),
@@ -26,7 +28,6 @@ get_started <- function(){
            "How measured" = character())
 
   # write table
-  write_csv(x = description_table, file = "data_dic/description_table.csv")
-
+  write_csv(x = description_table, file = paste0(path, "/description_table.csv"))
 
 }
